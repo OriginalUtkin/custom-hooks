d7b0c39cb2c880511b2e31786936b9e7ecf80934
Kirill <utkinkir@gmail.com>
2020-08-03 22:35:43 +0200

    check branch

--- update_version.sh
+++ update_version.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-red='\033[0;31m'
+yellow='\033[0;33m'
 reset_color='\033[0m'
 
-old_version=$(git diff VERSION | grep "^\-[1-9].[0-9].[0-9]" | cut -c2-6)
-new_version=$(git diff VERSION | grep "^\+[1-9].[0-9].[0-9]" | cut -c2-6)
+current_branch=$(git branch --show-current)
+echo "$current_branch"
 
 if [[ -z $(git diff VERSION) && -n $(git status | grep "$1*") ]]
 then
-    echo -e "${red} [ERROR] VERSION file wasn't changed. Current version.${reset_color}"
+    old_version=$(git diff VERSION | grep "^\-[1-9].[0-9].[0-9]" | cut -c2-6)
+    new_version=$(git diff VERSION | grep "^\+[1-9].[0-9].[0-9]" | cut -c2-6)
+    echo -e "${yellow} [WARNING] VERSION file wasn't changed. Version will be set automatically.${reset_color}"
+
     exit 1
 else
     exit 0
