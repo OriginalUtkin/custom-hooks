074af76cc992322e7d078cbba21b82458ab6e15f
Kirill <utkinkir@gmail.com>
2020-08-03 19:35:15 +0200

    1.0.0

--- update_change_log.sh
+++ update_change_log.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 red='\033[0;31m'
+reset_color='\033[0m'
 
 if [[ -z $(git diff CHANGELOG.$1) && -n $(git status | grep "$2*") ]]
 then
-    echo -e "${red} [ERROR] Changelog file was not changed! Add changes that were done in the repository to the CHANGELOG.$1"
+    echo -e "${red} [ERROR] Changelog file was not changed! Add changes that were done in the repository to the CHANGELOG.$1 ${reset_color}"
     exit 1
 else
     exit 0
--- update_version.sh
+++ update_version.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
+red='\033[0;31m'
+reset_color='\033[0m'
 
 old_version=$(git diff VERSION | grep "^\-[1-9].[0-9].[0-9]" | cut -c2-6)
 new_version=$(git diff VERSION | grep "^\+[1-9].[0-9].[0-9]" | cut -c2-6)
 
 if [[ -z $(git diff VERSION) && -n $(git status | grep "$1*") ]]
 then
-    echo "[ERROR] VERSION file wasn't changed. Current version "
+    echo -e "${red} [ERROR] VERSION file wasn't changed. Current version.${reset_color}"
     exit 1
 else
-    echo "[OK] VERSION file was changed: $old_version -> $new_version"
+    exit 0
 fi
-
-exit 0
