3694177f022d4a7470fa741c7783bf0d371694c9
Kirill <utkinkir@gmail.com>
2020-08-03 23:20:43 +0200

    Check current branch

--- update_version.sh
+++ update_version.sh
@@ -3,13 +3,11 @@ yellow='\033[0;33m'
 reset_color='\033[0m'
 
 current_branch=$(git branch --show-current)
-echo $current_branch
-
-changes=$(git status)
-echo $changes
+changed=$(git diff --name-only $current_branch $(git merge-base $current_branch "master"))
+echo $changed
 
 
-if [[ -z $(git diff VERSION) && -n $(git status | grep "$1*") ]]
+if [[ -z $(git diff VERSION) && -z $(grep "VERSION" <<< echo $changed) && (-n $(git status | grep "$1*") || -n $changed) ]]
 then
     old_version=$(git diff VERSION | grep "^\-[1-9].[0-9].[0-9]" | cut -c2-6)
     new_version=$(git diff VERSION | grep "^\+[1-9].[0-9].[0-9]" | cut -c2-6)
@@ -17,10 +15,6 @@ then
 
     exit 1
 else
-    current_branch=$(git branch --show-current)
-    echo -e $current_branch
 
-    changes=$(git status)
-    echo $changes
     exit 0
 fi
