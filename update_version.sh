fa47c961bd8db7f24fbab116f433b87aa0280756
Kirill <utkinkir@gmail.com>
2021-04-29 19:09:44 +0200

    Check what current branch is

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 red='\033[0;31m'
-
+1>&2 echo $(git branch)
 version_in_branch=$(cat VERSION)
 git worktree add master_state master
 version_in_master=$(cat master_state/VERSION)
