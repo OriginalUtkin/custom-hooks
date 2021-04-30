34a1cc8562df7adcec9cdf74788ca4531bc7563a
Kirill <utkinkir@gmail.com>
2021-04-30 13:51:18 +0200

    fetch master

--- update_version.sh
+++ update_version.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 red='\033[0;31m'
 
-version_in_branch=$(cat VERSION)
-echo $(git branch -a)
 
-git worktree add master_state remotes/origin/master
+version_in_branch=$(cat VERSION)
+echo "MR branch:" $version_in_branch
+
+git fetch -q origin master
+git worktree add -q master_state origin/master
+
 version_in_master=$(cat master_state/VERSION)
+
+echo "master:" $version_in_master
 
 if [[ "$version_in_branch" == "$version_in_master" ]]
 then
-  echo "${red} VERSION file is not updated ${reset_color}"
+  echo "VERSION file is not updated"
   exit 1
-else
-  echo $(git branch -a)
-  echo $(cat VERSION)
-  echo  $(cat master_state/VERSION)
-
-  exit 1
-
 fi
