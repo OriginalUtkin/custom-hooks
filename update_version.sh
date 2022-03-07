68d5f29876f0ac76aeaa132e215e6d091648a4bf
Kirill <utkinkir@gmail.com>
2022-03-07 20:25:41 +0100

    feat: get rid of poetry dependency in version checker

--- update_version.sh
+++ update_version.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-if [[ ("$2" == "poetry") || (("$2" == "") && ( -f pyproject.toml)) ]]; then
+if [[ ("$1" == "poetry") ]]; then
   version_file="pyproject.toml"
-  command="poetry version -s"
-elif [[ ("$2" == "version_file") || (("$2" == "") && ( -f VERSION)) ]]; then
+  command="grep -E '^version'|sed 's/\"//g'|sed -E 's/version[[:blank:]]?=[[:blank:]]?//g'"
+
+elif [[ ("$1" == "version_file") ]]; then
   version_file="VERSION"
-  command="cat VERSION"
+  command="grep -E '[0-9.]+'"
+
 else
-  echo "Version type '$2' not supported"
+  echo "Version type '$1' not supported"
   exit 1
 fi
 
-tfile=$(mktemp $version_file.XXXXXXXXX)
-mv $version_file $tfile
+version_in_branch=$(eval "cat $version_file | $command")
+
 git fetch -q origin master
-git checkout master -q -- $version_file
-version_in_master=$($command)
-mv $tfile $version_file
-version_in_branch=$($command)
+
+version_in_master=$(eval "git show origin/master:$version_file | $command")
 
 echo "Version in branch:" $version_in_branch
 echo "Version in master:" $version_in_master
 
-changed_files=$(git diff origin/master --summary --name-only |  grep -E $1)
+changed_files=$(git diff origin/master --summary --name-only |  grep -E $2)
 
 if [[ ("$version_in_branch" == "$version_in_master") && (! -z $changed_files)]]
 then
-  echo "VERSION file is not updated"
+  echo "Version in $version_file file is not updated"
   exit 1
 fi
